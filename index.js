0b3ebcaf5746eab101a5ccd3c81c892b8736389b
Arvind varma <av894883@gmail.com>
2023-10-01 11:09:16 +0530

    somethingChange

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const jsonServer = require("json-server");
-const dotenv = require("dotenv").config();
-const  server = jsonServer.create()
-const router = jsonServer.router("Notes.json")
-const middleweres = jsonServer.defaults();
- const port = process.env.Port||3000
- server.use(middleweres)
- server.use(router)
- server.listen(port,(res,err)=>console.log("json sarver start",port))
+// Import required packages
+const jsonServer = require("json-server"); // Import the json-server package
+const dotenv = require("dotenv").config(); // Load environment variables from a .env file
 
- 
+// Create an instance of the JSON server
+const server = jsonServer.create();
+
+// Create a router that will handle requests based on a JSON file (Notes.json in this case)
+const router = jsonServer.router("Notes.json");
+
+// Create middlewares for the JSON server
+const middlewares = jsonServer.defaults();
+
+// Define the port for the server, using an environment variable (PORT) if available, otherwise use port 3000
+const port = process.env.PORT || 3000;
+
+// Use the middlewares
+server.use(middlewares);
+
+// Use the router to handle API requests
+server.use(router);
+
+// Start the server and listen on the specified port
+server.listen(port, (res, err) => {
+  if (err) {
+    console.error("Error starting JSON server:", err);
+  } else {
+    console.log("JSON server started on port", port);
+  }
+});
